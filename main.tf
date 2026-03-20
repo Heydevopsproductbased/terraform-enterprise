0bcf7f0dd3aed56af4cadc7f1c21f38c15c6afd5
Priyanka <priyankashende055@gmail.com>
2026-03-20 17:06:10 +0000

    Add NSG with SSH and HTTP rules

--- main.tf
+++ main.tf
@@ -24,6 +24,45 @@ resource "azurerm_subnet" "main" {
   address_prefixes     = [var.subnet_address_prefix]
 }
 
+resource "azurerm_network_security_group" "main" {
+  name                = "nsg-${var.project_name}-${var.environment}"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  security_rule {
+    name                       = "Allow-SSH"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "Allow-HTTP"
+    priority                   = 200
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  tags = {
+    environment = var.environment
+  }
+}
+
+resource "azurerm_subnet_network_security_group_association" "main" {
+  subnet_id                 = azurerm_subnet.main.id
+  network_security_group_id = azurerm_network_security_group.main.id
+}
+
 resource "azurerm_public_ip" "main" {
   name                = "pip-${var.project_name}-${var.environment}"
   location            = azurerm_resource_group.main.location
