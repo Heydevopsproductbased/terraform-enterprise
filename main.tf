338983ca7040e4bcf7bc0767848ace9a47433646
Priyanka <priyankashende055@gmail.com>
2026-03-20 15:43:40 +0000

    Switch to Gen1 Ubuntu image for A2_v2

--- main.tf
+++ main.tf
@@ -72,9 +72,9 @@ resource "azurerm_linux_virtual_machine" "main" {
   }
 
   source_image_reference {
-    publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-jammy"
-    sku       = "22_04-lts-gen2"
-    version   = "latest"
-  }
+  publisher = "Canonical"
+  offer     = "UbuntuServer"
+  sku       = "18.04-LTS"
+  version   = "latest"
+}
 }
